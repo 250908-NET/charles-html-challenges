abb7c04d348e02a0f25979f7834455b74f59749c
Charles <charlestrangay@gmail.com>
2025-10-08 19:38:39 -0400

    feat(task): added task 13

---
+++ js-challenges/task-13/script.js
@@ -0,0 +1,12 @@
+function changeText() {
+    let text = document.getElementById("text");
+    text.style.color = "blue";
+    text.style.fontSize = "24px";
+}
+
+function resetText() {
+    let text = document.getElementById("text");
+    text.style.color = "black";
+    text.style.fontSize = "16px";
+}
+
